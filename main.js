18d34573af8dff307868f9725f68b69fe9aebdae
Skeleton-2010 <95185689+Skeleton-2010@users.noreply.github.com>
2023-03-01 19:45:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log("1-st");
+console.log("----------");
+let a = 3;
+
+while (a < 30) {
+  a++;
+  console.log(a);
+}
+console.log("The end");
+console.log("----------");
+
+console.log("2-nd");
+console.log("----------");
+let b = 5;
+
+while (a > -10) {
+  a--;
+  console.log(a);
+}
+console.log("The end");
+console.log("----------");
+
+let c;
+while (c != 0) {
+    c = prompt("Write '0'")
+    if (c != 0) {
+        alert("Please, write '0'!")
+    } else {
+        alert("Thank You!")
+    }
+}
